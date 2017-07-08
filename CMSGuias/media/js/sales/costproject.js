52891bf38a017c68566b7dcd2852c53a999647aa
Christian Valez <cvaldezchavez@gmail.com>
2017-07-08 13:41:56 -0500

    add costproject for project services

--- CMSGuias/media/js/sales/costproject.js
+++ CMSGuias/media/js/sales/costproject.js
@@ -1,6 +1,50 @@
-"use strict";
-exports.__esModule = true;
-var moduleServices_1 = require("../moduleServices");
-var angular = require("angular");
-var app = angular.module('app', ['ngCookies']);
-app.service('sproxy', moduleServices_1.Service.SProxy);
+// import * as angular from "angular";
+var SProxy = (function () {
+    function SProxy($http, $cookies) {
+        this.$http = $http;
+        this.$cookies = $cookies;
+        this.$http.defaults.headers.post['X-CSRFToken'] = $cookies['csrftoken'];
+        this.$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+    }
+    SProxy.prototype.get = function (uri, options) {
+        return this.$http.get(uri, { params: options });
+    };
+    SProxy.prototype.post = function (uri, options) {
+        return this.$http.post(uri, this.transformData(options), { transformRequest: angular.identity, headers: { 'Content-Type': undefined } });
+    };
+    SProxy.prototype.transformData = function (data) {
+        var form = new FormData();
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                var element = data[key];
+                form.append(key, element);
+            }
+        }
+        return form;
+    };
+    return SProxy;
+}());
+SProxy.$inject = ['$http', '$cookies'];
+var ControllerServiceProject = (function () {
+    function ControllerServiceProject(proxy) {
+        this.proxy = proxy;
+        console.log("hi! hello world!!!");
+        angular.element('.modal').modal();
+        this.getItemizer();
+    }
+    ControllerServiceProject.prototype.getItemizer = function () {
+        console.log('get itemizer');
+    };
+    return ControllerServiceProject;
+}());
+ControllerServiceProject.$inject = ['sproxy'];
+var apps = angular.module('app', ['ngCookies']);
+apps.service('sproxy', SProxy);
+apps.controller('controller', ControllerServiceProject);
+var httpConfigs = function ($httpProvider) {
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+};
+apps.config(httpConfigs);
+// Materialize.toast('hi!', 6000); 
--- CMSGuias/media/js/sales/costproject.ts
+++ CMSGuias/media/js/sales/costproject.ts
@@ -1,6 +1,68 @@
-import { Service } from '../moduleServices';
-import * as angular from "angular";
+// import * as angular from "angular";
 
-let app = angular.module('app', ['ngCookies']);
+interface IProxy {
+	get: (uri: string, options: object) => ng.IHttpPromise<any>;
 
-app.service('sproxy', Service.SProxy);
+	post: (url: string, options: object) => ng.IHttpPromise<any>;
+}
+
+class SProxy implements IProxy {
+
+	static $inject = ['$http', '$cookies'];
+
+	constructor(private $http: ng.IHttpService, private $cookies: ng.cookies.ICookieStoreService) {
+		this.$http.defaults.headers.post['X-CSRFToken'] = $cookies['csrftoken'];
+		this.$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+	}
+
+	get(uri: string, options: object) {
+		return this.$http.get(uri, {params: options});
+	}
+
+	post(uri: string, options: object) {
+		return this.$http.post(uri, this.transformData(options), {transformRequest: angular.identity, headers: {'Content-Type': undefined}})
+	}
+
+	private transformData(data: object): FormData {
+		let form = new FormData();
+		for (let key in data) {
+			if (data.hasOwnProperty(key)) {
+				let element = data[key];
+				form.append(key, element);
+			}
+		}
+		return form;
+	}
+}
+
+interface IController {
+	itemizer: object;
+	getItemizer(): void;
+
+}
+
+class ControllerServiceProject implements IController {
+	itemizer: object;
+	static $inject = ['sproxy']
+
+	constructor(private proxy: SProxy) {
+		console.log("hi! hello world!!!");
+		angular.element('.modal').modal();
+		this.getItemizer();
+	}
+
+	getItemizer(): void {
+		console.log('get itemizer')
+	}
+}
+
+let apps = angular.module('app', ['ngCookies']);
+apps.service('sproxy', SProxy);
+apps.controller('controller', ControllerServiceProject)
+let httpConfigs = ($httpProvider: ng.IHttpProvider) => {
+	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+	$httpProvider.defaults.xsrfCookieName = 'csrftoken';
+	$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+};
+apps.config(httpConfigs);
+// Materialize.toast('hi!', 6000);
