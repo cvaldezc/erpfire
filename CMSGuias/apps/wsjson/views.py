059b571417f7f2dd708d05af3c06bb3a62ad131e
Christian Valdez <cvaldezchavez@gmail.com>
2017-09-15 22:46:58 -0500

    add models Q

--- CMSGuias/apps/wsjson/views.py
+++ CMSGuias/apps/wsjson/views.py
@@ -14,7 +14,7 @@ from django.http import HttpResponse, Http404
 # from django.contrib.auth.models import User
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum
+from django.db.models import Sum, Q
 # from django.utils import simplejson
 from django.core.serializers.json import DjangoJSONEncoder
 from django.utils.decorators import method_decorator
